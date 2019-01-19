0da63a483bc25abd60046fa71af4dd85300d11ce
Dean Carver <car15014@byui.edu>
2019-01-19 11:33:26 -0700

    added style sheet

--- web/ponder02/homepage.php
+++ web/ponder02/homepage.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 	<title>About Me</title>
+  	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+ 	<link rel="stylesheet" type="text/css" href="ponder02/homestyle.css">
 </head>
 <body>
 <h1>Welcome to my page!</h1>

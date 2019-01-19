f76cc87572b1b725ed97f1211df3445e6b60fe45
Dean Carver <car15014@byui.edu>
2019-01-19 12:00:14 -0700

    added some styles and picture and info

--- web/ponder02/homepage.php
+++ web/ponder02/homepage.php
@@ -7,6 +7,12 @@
  	<link rel="stylesheet" type="text/css" href="homestyle.css">
 </head>
 <body>
-<h1>Welcome to my page!</h1>
+	<h1>Welcome to my page!</h1>
+	<table><tr><td>
+	<img src="me.jpg" alt="Me"></td>
+	<td>My name is Dean. One of my favorite things is Pokemon! I've been playing pokemon
+		since I was 5. My favorite Pokemon is <a href="https://www.pokemon.com/us/pokedex/flygon">Flygon.</a> I'm currently trying to catch as many shiny Pokemon as I can. I currently
+		have 97 shiny Pokemon></td></tr></table>
+
 </body>
 </html>

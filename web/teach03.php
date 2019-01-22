0befce85546723ad7061a811da447235e976613d
Dean Carver <car15014@byui.edu>
2019-01-22 09:37:04 -0700

    started it

---
+++ web/teach03.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		Name<input type="text" name="name">
+		Email<input type="text" name="email">
+	</form>
+</body>
+</html>

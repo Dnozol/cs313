239037195ab5cbfb2e801299dc9dac711eea1692
Dean Carver <car15014@byui.edu>
2019-01-22 09:44:34 -0700

    started it

--- web/teach03.php
+++ web/teach03.php
@@ -5,8 +5,13 @@
 </head>
 <body>
 	<form>
-		Name<input type="text" name="name">
-		Email<input type="text" name="email">
+		Name<input type="text" name="name"><br>
+		Email<input type="text" name="email"><br>
+		<input type="radio" name="major" value="Computer Science">Computer Science<br>
+		<input type="radio" name="major" value="Web Design and Development">Web Design and Development><br>
+		<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
+		<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
+		<textarea name="comments" rows="4" cols="50"></textarea>
 	</form>
 </body>
 </html>

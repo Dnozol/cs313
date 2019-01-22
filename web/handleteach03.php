9d04d130abca2dc31b42026208b009a95ce0a5b3
Dean Carver <car15014@byui.edu>
2019-01-22 10:29:39 -0700

    Continents

--- web/handleteach03.php
+++ web/handleteach03.php
@@ -14,8 +14,8 @@
 	echo "name: $name<br>";
 	echo "major: $major";
 	echo "<a href='mailto:" . $email . "?comments: " .  $comments ."'>$email</a><br>";
-	echo "$comments<br>"
-	echo "You have visited:<br>"
+	echo "$comments<br>";
+	echo "You have visited:<br>";
 	if(isset$_POST["c1"]) {
 		echo "$_POST["c1"]";
 	}

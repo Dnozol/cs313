d5dd3ebdcc07fbf1d5f044c51675a6f8b8d3cfca
Dean Carver <car15014@byui.edu>
2019-01-22 10:28:07 -0700

    Continents

--- web/handleteach03.php
+++ web/handleteach03.php
@@ -11,9 +11,14 @@
 	$major = $_POST["major"];
 	$comments = $_POST["comments"];
 
-	echo "name: $name";
-	echo "<a href='mailto:" . $email . "?comments: " .  $comments ."'>$email</a>";
-	echo "$comments"
+	echo "name: $name<br>";
+	echo "major: $major";
+	echo "<a href='mailto:" . $email . "?comments: " .  $comments ."'>$email</a><br>";
+	echo "$comments<br>"
+	echo "You have visited:<br>"
+	if(isset$_POST["c1"]) {
+		echo "$_POST["c1"]";
+	}
 
 ?>
 </body>
--- web/teach03.php
+++ web/teach03.php
@@ -12,6 +12,17 @@
 		<input type="radio" name="major" value="Computer Information Technology">Computer Information Technology<br>
 		<input type="radio" name="major" value="Computer Engineering">Computer Engineering<br>
 		<textarea name="comments" rows="4" cols="50"></textarea><br>
+		<br>
+		Which continents have you visited:
+		<br>
+		<input type="checkbox" name="c1" value="North America">NA<br>
+		<input type="checkbox" name="c1" value="South America">SA<br>
+		<input type="checkbox" name="c1" value="Europe">Eu<br>
+		<input type="checkbox" name="c4" value="Asia">As<br>
+		<input type="checkbox" name="c5" value="Australia">Au<br>
+		<input type="checkbox" name="c6" value="Africa">Af<br>
+		<input type="checkbox" name="c7" value="Antarctica">Ant<br>
+
 		<button type="submit">Submit</button>
 	</form>
 </body>

f4981fc155d0907876e60760aabc34fc54737354
Dean Carver <car15014@byui.edu>
2019-01-25 13:46:10 -0700

    Added basic table and style

---
+++ web/ponder03/inventory.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to the Pokemon Center!</title>
+  	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<h1>Welcome to the Pokemon Center</h1>
+		<form>
+		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
+			<tr><td>Item 1</td><td>$32.99</td><td><input type="checkbox" name="item1"></td></tr>
+			<tr><td>Item 1</td><td>$20.00</td><td><input type="checkbox" name="item2"></td></tr>
+			<tr><td>Item 1</td><td>$14.98</td><td><input type="checkbox" name="item3"></td></tr>
+			<tr><td>Item 1</td><td>$9.99</td><td><input type="checkbox" name="item4"></td></tr>
+			<tr><td>Item 1</td><td>$6.00</td><td><input type="checkbox" name="item5"></td></tr>
+			
+		</table>
+
+		<button type="submit">Proceed to purchase</button>
+		<button type="reset">Clear cart</button>
+	</form>
+</body>
+</html>

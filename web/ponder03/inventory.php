a4578316873163837d0c1b4f4614f9c5f268eea7
Dean Carver <car15014@byui.edu>
2019-01-26 14:31:10 -0700

    add to cart

--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -18,20 +18,27 @@ session_start();
 	<hr>
 		<form method="post" action="">
 		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
-			<tr><td>Item 1</td><td>$32.99</td><td><button type="submit" name="item[]" value="ball_collection">Add to cart</button></td></tr>
+			<!-- <tr><td>Item 1</td><td>$32.99</td><td><button type="submit" name="item[]" value="ball_collection">Add to cart</button></td></tr>
 			<tr><td>Item 2</td><td>$20.00</td><td><button type="submit" name="item[]" value="Giant_plush_snorlax">Add to cart</button></td></tr>
 			<tr><td>Item 3</td><td>$14.98</td><td><button onclick="addToSession(3, 14.98)">Add to cart</button></td></tr>
 			<tr><td>Item 4</td><td>$9.99</td><td><button onclick="addToSession(4, 9.99)">Add to cart</button></td></tr>
-			<tr><td>Item 5</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr>
+			<tr><td>Item 5</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr> -->
 			
+			<tr><td>Item 1</td><td>$32.99</td><td><input type="checkbox" name="item[]" value="ball_collection">Add to cart</td></tr>
+			<tr><td>Item 2</td><td>$20.00</td><td><input type="checkbox" name="item[]" value="Giant_plush_snorlax">Add to cart</td></tr>
+			<tr><td>Item 3</td><td>$14.98</td><td><input type="checkbox" name="item[]" value="Badge_collecion">Add to cart</td></tr>
+			<tr><td>Item 4</td><td>$9.99</td><td><input type="checkbox" name="item[]" value="Large_pikachu_plush">Add to cart</td></tr>
+			<tr><td>Item 5</td><td>$6.00</td><td><input type="checkbox" name="item[]" value="small_eevee_plush">Add to cart</td></tr>
+
 		</table>
 		<br><br>
-		<?php
+		<button type="submit">ViewCart</button>
+	</form>
+	<?php
 			$product=$_POST['item'];
 			array_push($_SESSION['cart'], $product);
 			echo "<br>The number of items= " . sizeof($_SESSION['cart']); 
 		?>
 		<a href="viewcart.php">View Your Cart</a>
-	</form>
 </body>
 </html>

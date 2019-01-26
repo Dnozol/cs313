14fc7010eaa7f93cdf1bd2f8f05de00e975197b7
Dean Carver <car15014@byui.edu>
2019-01-26 13:34:51 -0700

    add to cart

--- web/ponder03/addToCart.php
+++ web/ponder03/addToCart.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	$product=$_POST['item'];
+	@$product=$_POST['item'];
 	array_push($_SESSION['cart'], $product);
 	print_r($_SESSION['cart']);
 	header("Location: inventory.php");

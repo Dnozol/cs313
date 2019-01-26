1ab2c2630423c80178274d81304d2cdf85101bbc
Dean Carver <car15014@byui.edu>
2019-01-26 14:45:29 -0700

    adding images

--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -24,11 +24,16 @@ session_start();
 			<tr><td>Item 4</td><td>$9.99</td><td><button onclick="addToSession(4, 9.99)">Add to cart</button></td></tr>
 			<tr><td>Item 5</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr> -->
 			
-			<tr><td><img src="pokeballs.jpg" alt="pokeballs"></td><td>$32.99</td><td><input type="checkbox" name="item[]" value="ball_collection">Add to cart</td></tr>
-			<tr><td>Item 2</td><td>$20.00</td><td><input type="checkbox" name="item[]" value="Giant_plush_snorlax">Add to cart</td></tr>
-			<tr><td>Item 3</td><td>$14.98</td><td><input type="checkbox" name="item[]" value="Badge_collecion">Add to cart</td></tr>
-			<tr><td>Item 4</td><td>$9.99</td><td><input type="checkbox" name="item[]" value="Large_pikachu_plush">Add to cart</td></tr>
-			<tr><td>Item 5</td><td>$6.00</td><td><input type="checkbox" name="item[]" value="small_eevee_plush">Add to cart</td></tr>
+			<tr><td><img src="pokeballs.jpg" alt="pokeballs"><p>Pokeball Collection</p>
+			</td><td>$32.99</td><td><input type="checkbox" name="item[]" value="ball_collection">Add to cart</td></tr>
+			<tr><td><img src="plushSnorlax.jpg" alt="snorlax"><p>Giant Plush Snorlax</p>
+			</td><td>$20.00</td><td><input type="checkbox" name="item[]" value="Giant_plush_snorlax">Add to cart</td></tr>
+			<tr><td><img src="gymbadges.jpg" alt="badges"><p>Kanto Gym Badges</p>
+			</td><td>$14.98</td><td><input type="checkbox" name="item[]" value="Badge_collecion">Add to cart</td></tr>
+			<tr><td><img src="plushPikachu.jpg" alt="pikachu"><p>Large Plush Pikachu</p>
+			</td><td>$9.99</td><td><input type="checkbox" name="item[]" value="Large_pikachu_plush">Add to cart</td></tr>
+			<tr><td><img src="plushEevee" alt="eevee"><p>Small Eevee Plush</p>
+			</td><td>$6.00</td><td><input type="checkbox" name="item[]" value="small_eevee_plush">Add to cart</td></tr>
 
 		</table>
 		<br><br>

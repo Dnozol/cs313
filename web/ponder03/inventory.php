d058f1e8cf326be6cb5a967b4d5802a6819c6eef
Dean Carver <car15014@byui.edu>
2019-01-26 14:53:37 -0700

    adding images

--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -13,16 +13,13 @@ session_start();
 <body>
 	<?php
 		$_SESSION['cart']=array();
+		include 'product.php';
 	?>
 	<h1 class="center">Welcome to the Pokemon Center</h1>
 	<hr>
 		<form method="post" action="">
 		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
-			<!-- <tr><td>Item 1</td><td>$32.99</td><td><button type="submit" name="item[]" value="ball_collection">Add to cart</button></td></tr>
-			<tr><td>Item 2</td><td>$20.00</td><td><button type="submit" name="item[]" value="Giant_plush_snorlax">Add to cart</button></td></tr>
-			<tr><td>Item 3</td><td>$14.98</td><td><button onclick="addToSession(3, 14.98)">Add to cart</button></td></tr>
-			<tr><td>Item 4</td><td>$9.99</td><td><button onclick="addToSession(4, 9.99)">Add to cart</button></td></tr>
-			<tr><td>Item 5</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr> -->
+	
 			
 			<tr><td><img src="pokeballs.jpg" alt="pokeballs"><p>Pokeball Collection</p>
 			</td><td>$32.99</td><td><input type="checkbox" name="item[]" value="ball_collection">Add to cart</td></tr>
@@ -32,12 +29,12 @@ session_start();
 			</td><td>$14.98</td><td><input type="checkbox" name="item[]" value="Badge_collecion">Add to cart</td></tr>
 			<tr><td><img src="plushPikachu.jpg" alt="pikachu"><p>Large Plush Pikachu</p>
 			</td><td>$9.99</td><td><input type="checkbox" name="item[]" value="Large_pikachu_plush">Add to cart</td></tr>
-			<tr><td><img src="plushEevee" alt="eevee"><p>Small Eevee Plush</p>
+			<tr><td><img src="plushEevee.jpg" alt="eevee"><p>Small Eevee Plush</p>
 			</td><td>$6.00</td><td><input type="checkbox" name="item[]" value="small_eevee_plush">Add to cart</td></tr>
 
 		</table>
 		<br><br>
-		<button type="submit">ViewCart</button>
+		<button type="submit">Add to Cart</button>
 	</form>
 	<?php
 			$product=$_POST['item'];
---
+++ web/ponder03/product.php
@@ -0,0 +1,10 @@
+<?php
+	$product = array
+		(	
+		array("Pokeball Collection", 32.99),
+		array("Giant Snorlax Plush", 20.00),
+		array("Kanto Gym Badges", 14.98),
+		array("Large Plush Pikachu", 9.99),
+		array("Small Plush Eevee", 6.00)
+		);
+?>

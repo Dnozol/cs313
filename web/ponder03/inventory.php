4b50d068b685afabc0b7ba052bbaa1c48fed4a4d
Dean Carver <car15014@byui.edu>
2019-01-26 11:40:01 -0700

    stuff

--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -22,10 +22,10 @@ session_start();
 		<form>
 		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
 			<tr><td>Item 1</td><td>$32.99</td><td><button onclick="addToSession(1, 32.99)">Add to cart</button></td></tr>
-			<tr><td>Item 1</td><td>$20.00</td><td><button onclick="addToSession(2, 20.00)">Add to cart</button></td></tr>
-			<tr><td>Item 1</td><td>$14.98</td><td><button onclick="addToSession(3, 14.98)">Add to cart</button></td></tr>
-			<tr><td>Item 1</td><td>$9.99</td><td><button onclick="addToSession(4, 9.99)">Add to cart</button></td></tr>
-			<tr><td>Item 1</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr>
+			<tr><td>Item 2</td><td>$20.00</td><td><button onclick="addToSession(2, 20.00)">Add to cart</button></td></tr>
+			<tr><td>Item 3</td><td>$14.98</td><td><button onclick="addToSession(3, 14.98)">Add to cart</button></td></tr>
+			<tr><td>Item 4</td><td>$9.99</td><td><button onclick="addToSession(4, 9.99)">Add to cart</button></td></tr>
+			<tr><td>Item 5</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr>
 			
 		</table>
 		<br><br>

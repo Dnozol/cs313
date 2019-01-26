75a422fb652bdd4059761c8015666dbb83f4ceef
Dean Carver <car15014@byui.edu>
2019-01-26 14:05:42 -0700

    add to cart

--- web/ponder03/addToCart.php
+++ web/ponder03/addToCart.php
@@ -1,10 +1,21 @@
 <?php
 	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 
+<?php
 	$product=$_POST['item'];
 	array_push($_SESSION['cart'], $product);
 	print_r($_SESSION['cart']);
-	?>
-	<?php
-	header("Location: inventory.php");
+
+	//header("Location: inventory.php");
+
 ?>
+<a href="inventory.php">Inventory</a>
+</body>
+</html>
--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -19,7 +19,7 @@ session_start();
 		<form method="post" action="addToCart.php">
 		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
 			<tr><td>Item 1</td><td>$32.99</td><td><button type="submit" name="item" value="ball_collection">Add to cart</button></td></tr>
-			<tr><td>Item 2</td><td>$20.00</td><td><button onclick="addToSession(2, 20.00)">Add to cart</button></td></tr>
+			<tr><td>Item 2</td><td>$20.00</td><td><button type="submit" name="item2" value="Giant_plush_snorlax">Add to cart</button></td></tr>
 			<tr><td>Item 3</td><td>$14.98</td><td><button onclick="addToSession(3, 14.98)">Add to cart</button></td></tr>
 			<tr><td>Item 4</td><td>$9.99</td><td><button onclick="addToSession(4, 9.99)">Add to cart</button></td></tr>
 			<tr><td>Item 5</td><td>$6.00</td><td><button onclick="addToSession(5, 6.00)">Add to cart</button></td></tr>

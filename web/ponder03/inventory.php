cd91744cb5ae8da399fa2f7ffb07e3abe905584d
Dean Carver <car15014@byui.edu>
2019-01-26 11:49:31 -0700

    stuff

--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -8,18 +8,14 @@ session_start();
   	<meta charset="UTF-8">
  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
  	<link rel="stylesheet" type="text/css" href="ponder03.css">
- 	<script type="text/javascript">
- 		function addToSession(itemnum, price) {
- 			<?php
- 				$_SESSION["itemnum"] = "price";
- 			?>
- 		}
- 	</script>
+
 </head>
 <body>
+	<?php
+	?>
 	<h1 class="center">Welcome to the Pokemon Center</h1>
 	<hr>
-		<form>
+		<form method="post" action="inventory.php">
 		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
 			<tr><td>Item 1</td><td>$32.99</td><td><button onclick="addToSession(1, 32.99)">Add to cart</button></td></tr>
 			<tr><td>Item 2</td><td>$20.00</td><td><button onclick="addToSession(2, 20.00)">Add to cart</button></td></tr>
@@ -29,7 +25,7 @@ session_start();
 			
 		</table>
 		<br><br>
-		
+		<input type="submit" name="banana">
 		<a href="viewcart.php">View Your Cart</a>
 	</form>
 </body>

989afbbb1bcf8d11a3f7128559a55db668cfd8c3
Dean Carver <car15014@byui.edu>
2019-01-25 17:49:27 -0700

    Changed checkbox to link

--- web/ponder03/inventory.php
+++ web/ponder03/inventory.php
@@ -6,14 +6,15 @@
  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-	<h1>Welcome to the Pokemon Center</h1>
+	<h1 id="center">Welcome to the Pokemon Center</h1>
+	<hr>
 		<form>
 		<table><th><b>Item</b></th><th><b>Price</b></th><th>Add to cart?</th>
-			<tr><td>Item 1</td><td>$32.99</td><td><input type="checkbox" name="item1"></td></tr>
-			<tr><td>Item 1</td><td>$20.00</td><td><input type="checkbox" name="item2"></td></tr>
-			<tr><td>Item 1</td><td>$14.98</td><td><input type="checkbox" name="item3"></td></tr>
-			<tr><td>Item 1</td><td>$9.99</td><td><input type="checkbox" name="item4"></td></tr>
-			<tr><td>Item 1</td><td>$6.00</td><td><input type="checkbox" name="item5"></td></tr>
+			<tr><td>Item 1</td><td>$32.99</td><td><a href="">Add to cart</a></td></tr>
+			<tr><td>Item 1</td><td>$20.00</td><td><a href="">Add to cart</a></td></tr>
+			<tr><td>Item 1</td><td>$14.98</td><td><a href="">Add to cart</a></td></tr>
+			<tr><td>Item 1</td><td>$9.99</td><td><a href="">Add to cart</a></td></tr>
+			<tr><td>Item 1</td><td>$6.00</td><td><a href="">Add to cart</a></td></tr>
 			
 		</table>
 

2e15e2e206113f5913dc8cfffed21cffbcbd00f2
Dean Carver <car15014@byui.edu>
2019-01-26 19:26:16 -0700

    display test

--- web/ponder03/product.php
+++ web/ponder03/product.php
@@ -3,7 +3,7 @@
 		(	
 		array("<img src='pokeballs.jpg' alt='pokeballs'>","Pokeball Collection", 32.99, "ball_collection"),
 		array("<img src='plushSnorlax.jpg' alt='Snorlax'>","Giant Snorlax Plush", 24.75, "giant_plush_snorlax"),
-		array("<img src='gybadges.jpg' alt='badges'>", "Kanto Gym Badges", 14.98, "badge_colletion"),
+		array("<img src='gymbadges.jpg' alt='badges'>", "Kanto Gym Badges", 14.98, "badge_collection"),
 		array("<img src='plushPikachu.jpg' alt='pikachu'>", "Large Plush Pikachu", 9.99, "large_pikachu_plush"),
 		array("<img src='plushEevee.jpg' alt='eevee'>", "Small Plush Eevee", 6.99, "small_eevee_plush")
 		);

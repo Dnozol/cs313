37e144d4ce32a257326b8d0f65759035d84824db
Dean Carver <car15014@byui.edu>
2019-02-09 21:35:38 -0700

    'added' teamEditor page...

--- web/Ponder05/teamEditor.php
+++ web/Ponder05/teamEditor.php
@@ -1,25 +1,23 @@
 <?php require('db.php'); ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome <?php echo htmlspecialchars($_POST["username"]); ?> </title>
+	<title>Team Editor</title>
 	<link rel="stylesheet" type="text/css" href="ponder05.css">
 	<meta charset="utf-8"/>
  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body>
-	<?php include 'banners.php';?>
+	<?php include('banners.php'); ?>
 	<div class="flex-container">
 		<div class="sidebar">
-			<button>Remove Team(s)</button>
-			<button>Add Team</button>
-			<button>Edit Team</button>
-		</div><!--end of sidebar flexbox-->
+
+		</div> <!--end of sidebar flexbox-->
 		<div class="main">
-			<h1>Current Teams</h1>
+			
 		</div><!--end of main flexbox-->
-	</div><!--end of flex container
+	</div>
+
 </body>
 </html>

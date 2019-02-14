4563bc44420c3414a1e2e68cd0673646a18d7751
Dean Carver <car15014@byui.edu>
2019-02-14 10:50:01 -0700

    moved files

--- 2_14classExample.php
+++ 2_14classExample.php
@@ -10,9 +10,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
+	<!-- <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv-"X-UA-Compatible" content="ie=edge">
+	<meta http-equiv-"X-UA-Compatible" content="ie=edge"> -->
 	<title></title>
 </head>
 <body>

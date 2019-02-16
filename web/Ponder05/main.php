a7b8d27d09947d0ad90cea51795c935cdfb7bd4d
Dean Carver <car15014@byui.edu>
2019-02-16 15:15:57 -0700

    Maybe now it's mixed

--- web/Ponder05/main.php
+++ web/Ponder05/main.php
@@ -5,9 +5,9 @@
 	$query = 'SELECT team_name, pokemon_name FROM team_pokemon tp
 			  JOIN team t ON t.team_id = tp.team_id
 			  JOIN pokemon p on p.pokemon_id = tp.pokemon_id;';
-	$stmt = $db->prepare($query);
-	$stmt->execute();
-	$teams = $stmt->fecthAll(PDO::FECTH_ASSOC);
+	// $stmt = $db->prepare($query);
+	// $stmt->execute();
+	// $teams = $stmt->fecthAll(PDO::FECTH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>

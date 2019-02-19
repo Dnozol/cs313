6201aab9b7f744f9847e24b14370538b1906122f
Dean Carver <car15014@byui.edu>
2019-02-19 11:25:17 -0700

    added regestration

--- web/Ponder05/createTrainer.php
+++ web/Ponder05/createTrainer.php
@@ -1,28 +1,28 @@
 <?php
 	require_once('db.php'); 
 	$db = get_db();
-	$query = 'INSERT INTO trainer (trainer_name, password) VALUES (:trainer_name, :password);';
+	//$query = 'INSERT INTO trainer (trainer_name, password) VALUES (:trainer_name, :password);';
 
-	if (!isset($_POST['trainer_name']) || strlen($_POST['trainer_name']) == 0) {
-		header('Location: register.php');
-		die();
-	} else if (!isset($_POST['password']) || strlen($_POST['password']) == 0) {
-		header('Location: register.php');
-		die();
-	} else if ($_POST['password'] != $_POST['confirm']) {
-		header('Location: register.php');
-		die();
-	} 
+	// if (!isset($_POST['trainer_name']) || strlen($_POST['trainer_name']) == 0) {
+	// 	header('Location: register.php');
+	// 	die();
+	// } else if (!isset($_POST['password']) || strlen($_POST['password']) == 0) {
+	// 	header('Location: register.php');
+	// 	die();
+	// } else if ($_POST['password'] != $_POST['confirm']) {
+	// 	header('Location: register.php');
+	// 	die();
+	// } 
 
-	$trainer_name = $_POST['trainer_name'];
-	$password = password_hash($_POST['password']);
+	// $trainer_name = $_POST['trainer_name'];
+	// $password = password_hash($_POST['password']);
 
 
-	$stmt = $db->prepare($query);
-	$stmt->bindValue(':trainer_name', $trainer_name, PDO::PARAM_STR);
-	$stmt->bindValue(':password', $password, PDO::PARAM_STR);
-	$stmt->execute();
+	// $stmt = $db->prepare($query);
+	// $stmt->bindValue(':trainer_name', $trainer_name, PDO::PARAM_STR);
+	// $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+	// $stmt->execute();
 
-	header('Location: signin.php');
-	die();
+	// header('Location: signin.php');
+	// die();
 ?>

2732145ace41dd547aa443f87b6fc2fc0db67036
Dean Carver <car15014@byui.edu>
2019-02-21 08:19:51 -0700

    Hopefully fixed create trainer bug

--- web/Ponder05/createTrainer.php
+++ web/Ponder05/createTrainer.php
@@ -2,7 +2,7 @@
 	require_once('db.php'); 
 
 	$trainer_name = $_POST['trainer_name'];
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 	
 	echo "$trainer_name";
 
@@ -22,13 +22,9 @@
 	echo "Part2";
 
 	$stmt = $db->prepare($query);
-	echo "stop1";
 	$stmt->bindValue(':trainer_name', $trainer_name, PDO::PARAM_STR);
-	echo "stop2";
 	$stmt->bindValue(':password', $password, PDO::PARAM_STR);
-	echo "stop3";
 	$stmt->execute();
-	echo "stop4";
 	header('Location: signin.php');
 	die();
 ?>

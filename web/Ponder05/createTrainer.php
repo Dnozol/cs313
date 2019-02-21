488145efa87cc39bfa6a2113cd552b94431f260e
Dean Carver <car15014@byui.edu>
2019-02-21 08:14:35 -0700

    Hopefully fixed create trainer bug

--- web/Ponder05/createTrainer.php
+++ web/Ponder05/createTrainer.php
@@ -22,11 +22,11 @@
 	echo "Part2";
 
 	$stmt = $db->prepare($query);
-	echo "stop1";
-	$stmt->bindValue(':trainer_name', $trainer_name, PDO::PARAM_STR);
-	echo "stop2";
-	$stmt->bindValue(':password', $password, PDO::PARAM_STR);
-	echo "stop3";
+	// echo "stop1";
+	// $stmt->bindValue(':trainer_name', $trainer_name, PDO::PARAM_STR);
+	// echo "stop2";
+	// $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+	// echo "stop3";
 	$stmt->execute();
 	echo "stop4";
 	header('Location: signin.php');

b1a8ea77784f2ccad14f5ebb263d397852df642f
Dean Carver <car15014@byui.edu>
2019-02-23 16:34:11 -0700

    added user lookup

--- web/Ponder05/confirmSignin.php
+++ web/Ponder05/confirmSignin.php
@@ -5,16 +5,10 @@
 	$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 	
 
-	$echo = "ready<br>";
-	$echo = "show this!";
-	for ($i = 0; $i < 10; $i++) {
-		echo "$i<br>";
-	}
-
 	$db = get_db();
 	$query = 'SELECT trainer_name FROM trainer WHERE trainer_name = $trainer_name';
 
-	// $echo "$query";
+	$echo "$query";
 	
 	// $stmt = $db->prepare($query);
 	// $stmt->bindValue(':trainer_name', $trainer_name, PDO::PARAM_STR);

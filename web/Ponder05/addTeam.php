ba92ef3db435654ee1fe1b5039bef67c997a1d94
Dean Carver <car15014@byui.edu>
2019-02-25 11:45:37 -0700

    add pokemon selection for addTeam

--- web/Ponder05/addTeam.php
+++ web/Ponder05/addTeam.php
@@ -18,7 +18,29 @@
  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
  	<meta http-equiv="X-UA-Compatible" content="ie=edge">
  	<script type="text/javascript">
- 		
+ 		function repeatList(repeat) {
+ 			for(var i = 0; i < repeat; i++){
+
+	 			<?php
+	 				echo "<select id='pokemon_select'>";
+					echo "<option value='empty'>Empty Slot</option>";
+
+					foreach($pokemon_list as $pokemon) {
+						$pokemon_name = $pokemon['pokemon_name'];
+
+						echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
+						echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
+						echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
+						echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
+						echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
+						echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
+					
+					}
+					echo "</select>";
+		
+				?>
+			} // end for loop
+		}
  	</script>
 </head>
 <body>
@@ -31,25 +53,7 @@
 		</div> <!--end of sidebar flexbox-->
 		<div class="main">
 		<h1>Add New team</h1>
-
-			<?php
-				foreach($pokemon_list as $pokemon) {
-
-					echo "<select id='pokemon_select'>";
-					echo "<option value='empty'>Empty Slot</option>";
-					$pokemon_name = $pokemon['pokemon_name'];
-
-
-					echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
-					echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
-					echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
-					echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
-					echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
-					echo "<option value='" . $pokemon_name . "'>" . $pokemon_name . "</option>";
-					
-					echo "</select>";
-				}
-			?>
+		<script type="text/javascript">repeatList(6);</script>
 		<br>
 		<input type="submit" value="Add Team">
 		</div><!--end of main flexbox-->

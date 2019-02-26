356ff8326822e8447b6f4373ee4042c7b8222f64
Dean Carver <car15014@byui.edu>
2019-02-25 17:58:00 -0700

    add team to database

--- web/Ponder05/teamToDb.php
+++ web/Ponder05/teamToDb.php
@@ -13,18 +13,8 @@
 	$team_name = htmlspecialchars($_POST['team_name']);
 	$team_pokemon = $_POST['pokemon_name']
 
-	$queryName = 'INSERT INTO team (team_name) VALUES ("$team_name");'; 
-	$stmt = $db->prepare($queryName);
+	$query = 'INSERT INTO team (team_name) VALUES ("' . $team_name. '");'; 
+	$stmt = $db->prepare($query);
 	$stmt->execute();
 	$pokemon_list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-INSERT INTO team_pokemon
-VALUES
-( DEFAULT
-, (SELECT team_id FROM team WHERE team_name = 'EXAMPLE2')
-, (SELECT pokemon_id FROM pokemon WHERE pokemon_name = 'Bulbasaur')
-);
-
-INSERT INTO team 
-(team_name) VALUES ('EXAMPLE2');
